29aeaf5b478b98f8e7595c1080fc548782d582ea
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2021-12-01 01:33:59 -0800

    day1-part2: destructuring abstraction

--- day1.lisp
+++ day1.lisp
@@ -14,8 +14,18 @@
   (loop for (prev next) on input while next
         count (> next prev)))
 
+;; Consider sums of a three-measurement sliding window. Count the number of
+;; times the sum of measurements in this sliding window increases from the
+;; previous sum.
+(defun day1-part2 (input)
+  (let ((sums (loop for (m1 m2 m3) on input while m3
+                    collect (+ m1 m2 m3))))
+    (day1-part1 sums)))
+
 ;; TODO macro-ify this or something
 (let ((example (day1-input "example"))
       (input (day1-input "input")))
   (assert (= (day1-part1 example) 7))
-  (format t "day1-part1: ~a~%" (day1-part1 input)))
+  (assert (= (day1-part2 example) 5))
+  (format t "day1-part1: ~a~%" (day1-part1 input))
+  (format t "day1-part2: ~a~%" (day1-part2 input)))
