b0f4f730fb16582acd304c308fd38659964c27cb
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2021-12-01 02:31:19 -0800

    day1: make running on player-specific input optional, plus some cleanup
    Instead of erroring if the player-specific input file doesn't exist, just
    ignore it. Also, rename DAY-INPUT to DAY-FILE to avoid unnecessarily
    overloading the term "input".

--- day1.lisp
+++ day1.lisp
@@ -3,10 +3,10 @@
 ;; Each line is a measurement of the sea floor depth as the sweep looks further
 ;; and further away from the submarine.
 (defun day1-input (file)
-  (with-open-file (stream (day-input 1 file))
-    (loop for line = (read-line stream nil)
-          while line
-          collect (parse-integer line))))
+  (with-open-file (stream (day-file 1 file) :if-does-not-exist nil)
+    (when stream
+      (loop for line = (read-line stream nil) while line
+            collect (parse-integer line)))))
 
 ;; Count the number of times a depth measurement increases from the previous
 ;; measurement.
@@ -23,9 +23,10 @@
     (day1-part1 sums)))
 
 ;; TODO macro-ify this or something
-(let ((example (day1-input "example"))
-      (input (day1-input "input")))
+(let ((example (day1-input "example")))
   (assert (= (day1-part1 example) 7))
-  (assert (= (day1-part2 example) 5))
-  (format t "day1-part1: ~a~%" (day1-part1 input))
-  (format t "day1-part2: ~a~%" (day1-part2 input)))
+  (assert (= (day1-part2 example) 5)))
+(let ((input (day1-input "input")))
+  (when input
+    (format t "day1-part1: ~a~%" (day1-part1 input))
+    (format t "day1-part2: ~a~%" (day1-part2 input))))
