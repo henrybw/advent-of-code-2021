352394a6fc8f76aba73893385cf20f92efbf2b9c
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2021-12-01 21:48:00 -0800

    day2-part2: slightly different summing clauses

--- day2.lisp
+++ day2.lisp
@@ -30,9 +30,31 @@
         sum dy into y
         finally (return (* x y))))
 
+;; In addition to horizontal position and depth, you'll also need to track a
+;; third value, aim, which also starts at 0. The commands also mean something
+;; entirely different than you first thought:
+;;
+;;  - "down X" increases your aim by X units.
+;;  - "up X" decreases your aim by X units.
+;;  - "forward X" does two things:
+;;    - It increases your horizontal position by X units.
+;;    - It increases your depth by your aim multiplied by X.
+;;
+;; Using this new interpretation of the commands, calculate the horizontal
+;; position and depth you would have after following the planned course. What do
+;; you get if you multiply your final horizontal position by your final depth?
+(defmethod solve ((day (eql 2)) (part (eql 2)) input)
+  (loop for (dx dy) in input while dy
+        sum dx into x
+        sum dy into aim
+        sum (* aim dx) into y
+        finally (return (* x y))))
+
 (let ((example (parse 2 "example")))
-  (assert (= (solve 2 1 example) 150)))
+  (assert (= (solve 2 1 example) 150))
+  (assert (= (solve 2 2 example) 900)))
 
 (let ((input (parse 2 "input")))
   (when input
-    (format t "day2-part1: ~a~%" (solve 2 1 input))))
+    (format t "day2-part1: ~a~%" (solve 2 1 input))
+    (format t "day2-part2: ~a~%" (solve 2 2 input))))
