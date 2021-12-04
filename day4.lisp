27ba38c78578ae5bfb1f0aeaae64da91a101ec71
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2021-12-04 04:50:10 -0800

    day4-part2: simultaneous winners and mutable state made this tricky

--- day4.lisp
+++ day4.lisp
@@ -36,7 +36,7 @@
                    always (aref state row col)
                    finally (return-from winning-board-p t)))))
 
-(defun find-winner (numbers boards states)
+(defun find-first-winner (numbers boards states)
   (dolist (target numbers)
     (let ((winner (loop for board in boards
                         for state in states
@@ -45,6 +45,29 @@
                           do (return (list target board state)))))
       (when winner
         (return winner)))))
+
+(defun find-last-winner (numbers boards states)
+  (let (last-winners won-boards)
+    (dolist (target numbers)
+      (loop for board in boards
+            for state in states
+            do (mark-board target board state))
+      ;; multiple boards can win simultaneously
+      (let ((winners (loop for board in boards
+                           for state in states
+                           unless (member board won-boards)
+                             when (winning-board-p state)
+                               collect (list target board state))))
+        (dolist (winner winners)
+          (destructuring-bind (target board state) winner
+            (let ((state-copy (make-array '(5 5) :element-type 'boolean
+                                                 :initial-element nil)))
+              ;; copy the board state as it was when the win occurred
+              (dotimes (i (array-total-size state))
+                (setf (row-major-aref state-copy i) (row-major-aref state i)))
+              (pushnew (list target board state-copy) last-winners)
+              (pushnew board won-boards))))))
+    (first last-winners)))
 
 (defun sum-unmarked (board state)
   (loop for board-row in board
@@ -58,21 +81,32 @@
 
 ;; The score of the winning board is calculated by finding the sum of all
 ;; unmarked numbers, then multiplying that sum by the number that was just
-;; called when the board won. To guarantee victory against the giant squid,
-;; figure out which board will win first. What will your final score be if you
-;; choose that board?
-(defmethod solve ((day (eql 4)) (part (eql 1)) input)
+;; called when the board won.
+(defun winning-board-score (which-winner input)
   (destructuring-bind (numbers boards) input
     (let ((states (loop for i below (length boards)
                         collect (make-array '(5 5) :element-type 'boolean
                                                    :initial-element nil))))
       (destructuring-bind (last-number board state)
-          (find-winner numbers boards states)
+          (funcall which-winner numbers boards states)
         (* last-number (sum-unmarked board state))))))
 
+;; To guarantee victory against the giant squid, figure out which board will win
+;; first. What will your final score be if you choose that board?
+(defmethod solve ((day (eql 4)) (part (eql 1)) input)
+  (winning-board-score #'find-first-winner input))
+
+;; On the other hand, it might be wise to try a different strategy: let the
+;; giant squid win. Figure out which board will win last. Once it wins, what
+;; would its final score be?
+(defmethod solve ((day (eql 4)) (part (eql 2)) input)
+  (winning-board-score #'find-last-winner input))
+
 (let ((example (parse 4 "example")))
-  (assert (= (solve 4 1 example) 4512)))
+  (assert (= (solve 4 1 example) 4512))
+  (assert (= (solve 4 2 example) 1924)))
 
 (let ((input (parse 4 "input")))
   (when input
-    (format t "day4-part1: ~a~%" (solve 4 1 input))))
+    (format t "day4-part1: ~a~%" (solve 4 1 input))
+    (format t "day4-part2: ~a~%" (solve 4 2 input))))
