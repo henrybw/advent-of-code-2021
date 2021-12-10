2f3b2e9928a19f634d18dfa469f4abc12f07d701
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2021-12-10 02:54:52 -0800

    day10-part2: score the leftovers on the stack

--- day10.lisp
+++ day10.lisp
@@ -27,9 +27,41 @@
 (defmethod solve ((day (eql *day*)) (part (eql 1)) input)
   (loop for line in input sum (error-score line)))
 
+(defun incomplete-score (line)
+  (let (token-stack)
+    (dolist (token line)
+      (if (find token "([{<")
+          (push token token-stack)
+          (let ((open-token (pop token-stack)))
+            (when (or (and (char/= open-token #\() (char= token #\)))
+                      (and (char/= open-token #\[) (char= token #\]))
+                      (and (char/= open-token #\{) (char= token #\}))
+                      (and (char/= open-token #\<) (char= token #\>)))
+              (return-from incomplete-score 0)))))
+    (let ((score 0))
+      (dolist (token token-stack)
+        (setf score (+ (* score 5) (cond ((char= token #\() 1)
+                                         ((char= token #\[) 2)
+                                         ((char= token #\{) 3)
+                                         ((char= token #\<) 4)
+                                         (t 0)))))
+      score)))
+
+;; Find the completion string for each incomplete line, score the completion
+;; strings, and sort the scores. What is the middle score?
+(defmethod solve ((day (eql *day*)) (part (eql 2)) input)
+  (let ((scores (sort (loop for line in input
+                            for score = (incomplete-score line)
+                            unless (= score 0)
+                              collect score)
+                      #'<)))
+    (nth (floor (length scores) 2) scores)))
+
 (let ((example (parse *day* "example")))
-  (assert (= (solve *day* 1 example) 26397)))
+  (assert (= (solve *day* 1 example) 26397))
+  (assert (= (solve *day* 2 example) 288957)))
 
 (let ((input (parse *day* "input")))
   (when input
-    (format t "day~a-part1: ~a~%" *day* (solve *day* 1 input))))
+    (format t "day~a-part1: ~a~%" *day* (solve *day* 1 input))
+    (format t "day~a-part2: ~a~%" *day* (solve *day* 2 input))))
