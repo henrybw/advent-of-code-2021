f9e0da09c76f20c344b33b71e78149cd94be79d4
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2021-12-12 01:47:12 -0800

    day12-part2: keep track of one exception to the "no revisit" rule

--- day12.lisp
+++ day12.lisp
@@ -25,9 +25,31 @@
                      sum (explore adjacent visited))))
     (explore "start" nil)))
 
+;; After reviewing the available paths, you realize you might have time to visit
+;; a single small cave twice. However, the caves named "start" and "end" can
+;; only be visited exactly once each. Given these new rules, how many paths
+;; through this cave system are there?
+(defmethod solve ((day (eql *day*)) (part (eql 2)) node->adjacent)
+  (labels ((explore (node visited revisit-exception)
+             (when (string= node "end")
+               (return-from explore 1))
+             (unless (every #'upper-case-p node)
+               (pushnew node visited :test #'equal))
+             (loop for adjacent in (gethash node node->adjacent)
+                   if (and (member adjacent visited :test #'equal)
+                           (null revisit-exception)
+                           (string/= adjacent "start"))
+                     sum (explore adjacent visited adjacent)
+                   else
+                     unless (member adjacent visited :test #'equal)
+                       sum (explore adjacent visited revisit-exception))))
+    (explore "start" nil nil)))
+
 (let ((example (parse *day* "example")))
-  (assert (= (solve *day* 1 example) 10)))
+  (assert (= (solve *day* 1 example) 10))
+  (assert (= (solve *day* 2 example) 36)))
 
 (let ((input (parse *day* "input")))
   (when input
-    (format t "day~a-part1: ~a~%" *day* (solve *day* 1 input))))
+    (format t "day~a-part1: ~a~%" *day* (solve *day* 1 input))
+    (format t "day~a-part2: ~a~%" *day* (solve *day* 2 input))))
