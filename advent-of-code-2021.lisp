ba43ba724eb62b893fd24415c00fda6d4231f368
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2021-12-14 01:13:40 -0800

    clean up parse/solve generic definitions

--- advent-of-code-2021.lisp
+++ advent-of-code-2021.lisp
@@ -7,6 +7,15 @@
   (./ (make-pathname :directory (list :relative "day" (write-to-string n))
                      :name path)))
 
+(defgeneric parse (day input))
+
+(defmethod parse ((day integer) (input string))
+  (with-open-file (stream (day-pathname day input) :if-does-not-exist nil)
+    (when stream
+      (parse day stream))))
+
+(defgeneric solve (day part input))
+
 ;; Returns a 2D matrix of integers from the given list of strings LINES, each
 ;; line consisting of digit characters.
 (defun parse-matrix (lines)
@@ -28,12 +37,3 @@
         for adj-col = (+ col col-offset)
         when (array-in-bounds-p matrix adj-row adj-col)
           collect (list adj-row adj-col)))
-
-(defgeneric parse (day input))
-
-(defmethod parse ((day integer) (input string))
-  (with-open-file (stream (day-pathname day input) :if-does-not-exist nil)
-    (when stream
-      (parse day stream))))
-
-(defgeneric solve (day part input))
