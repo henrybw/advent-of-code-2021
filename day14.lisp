0f151ebbfba0df1bccfcd5b2ca38cfe41fa56fff
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2023-12-06 19:53:42 -0800

    day14-part2: finally solve this (by keeping track of the element counts)

--- day14.lisp
+++ day14.lisp
@@ -2,53 +2,64 @@
 
 (defparameter *day* 14)
 
-;; Returns a list of (POLYMER-TEMPLATE PAIR-INSERTIONS), where POLYMER-TEMPLATE
-;; is a string, and PAIR-INSERTIONS is hash table mapping pairs of characters,
-;; e.g. (#\A . #\B), to the character to insert between them.
-(defmethod parse ((day (eql *day*)) (input stream))
-  (let* ((lines (uiop:slurp-stream-lines input))
-         (polymer-template (first lines))
-         (pair-insertions (make-hash-table :test #'equal)))
-    (mapcar (lambda (rule) (destructuring-bind (pair insertion)
-                               (split-by rule " -> ")
-                             (setf (gethash (cons (char pair 0) (char pair 1))
-                                            pair-insertions)
-                                   (char insertion 0))))
-            (subseq lines 2))
-    (list polymer-template pair-insertions)))
-
 (defun ensure-incf (table key)
   (alexandria:ensure-gethash key table 0)
   (incf (gethash key table)))
 
-(defun remove-decf (table key)
-  (if (= (gethash key table) 1)
-      (remhash key table)
-      (decf (gethash key table))))
+(defun ensure-add (table key val)
+  (alexandria:ensure-gethash key table 0)
+  (setf (gethash key table) (+ (gethash key table) val)))
 
-(defun most-least-diff-after (steps polymer rules)
-  (let ((elem-counts (make-hash-table))
-        (pair-counts (make-hash-table :test #'equal)))
-    (loop for elem in polymer
-          do (ensure-incf elem-counts elem))
-    (loop for (a b) on polymer
-          when b do (ensure-incf pair-counts (cons a b)))
+;; Returns a list of (ELEM-COUNTS PAIR-COUNTS PAIR-EXPANSIONS), where
+;; ELEM-COUNTS is a hash table mapping characters to a count of their
+;; appearances in the polymer template, PAIR-COUNTS is a hash table mapping
+;; character pairs (e.g. (#\A . #\B)) to a count of their appearances in the
+;; polymer template, and PAIR-EXPANSIONS is hash table mapping character pairs
+;; to a list of the two other character pairs they expand to.
+(defmethod parse ((day (eql *day*)) (input stream))
+  (let* ((lines (uiop:slurp-stream-lines input))
+         (template (coerce (first lines) 'list))
+         (elem-counts (make-hash-table))
+         (pair-counts (make-hash-table :test #'equal))
+         (pair-expansions (make-hash-table :test #'equal)))
+    (loop for e in template
+          do (ensure-incf elem-counts e))
+    (loop for (e1 e2) on template while e2
+          do (ensure-incf pair-counts (cons e1 e2)))
+    (mapcar (lambda (rule) (destructuring-bind (pair insertion)
+                               (split-by rule " -> ")
+                             (setf (gethash (cons (char pair 0) (char pair 1))
+                                            pair-expansions)
+                                   (list
+                                    (cons (char pair 0) (char insertion 0))
+                                    (cons (char insertion 0) (char pair 1))))))
+            (subseq lines 2))
+    (list elem-counts pair-counts pair-expansions)))
+
+(defun apply-pair-insertions (steps elem-counts pair-counts pair-expansion)
+  (let ((pair-changes (make-hash-table :test #'equal)))
     (dotimes (i steps)
-      (let (pairs-inserted pairs-removed)
-        (loop for pair being the hash-key using (hash-value insertion) of rules
-              do (let ((occurs (gethash pair pair-counts)))
-                   (when occurs
-                     (let ((insertion (gethash pair rules)))
-                       (when insertion
-                         (dotimes (j occurs)
-                           (ensure-incf elem-counts insertion)
-                           (push (cons (car pair) insertion) pairs-inserted)
-                           (push (cons insertion (cdr pair)) pairs-inserted)
-                           (push pair pairs-removed)))))))
-        (dolist (pair pairs-inserted)
-          (ensure-incf pair-counts pair))
-        (dolist (pair pairs-removed)
-          (remove-decf pair-counts pair))))
+      (clrhash pair-changes)
+      (maphash
+       (lambda (old-pair count)
+         (assert (>= count 0))
+         (let ((expansion (gethash old-pair pair-expansion)))
+           (when expansion
+             (destructuring-bind (new-pair1 new-pair2) expansion
+               (ensure-add pair-changes new-pair1 count)
+               (ensure-add pair-changes new-pair2 count)
+               (ensure-add pair-changes old-pair (- count))
+
+               (assert (eql (cdr new-pair1) (car new-pair2)))
+               (ensure-add elem-counts (cdr new-pair1) count)))))
+       pair-counts)
+      (maphash
+       (lambda (pair delta)
+         (multiple-value-bind (old-count presentp) (gethash pair pair-counts)
+           (setf (gethash pair pair-counts)
+                 (if presentp (+ old-count delta) delta))))
+       pair-changes))
+
     (loop for occurs in (alexandria:hash-table-values elem-counts)
           maximize occurs into most
           minimize occurs into least
@@ -59,13 +70,25 @@
 ;; quantity of the most common element and subtract the quantity of the least
 ;; common element?
 (defmethod solve ((day (eql *day*)) (part (eql 1)) input)
-  (destructuring-bind (template rules) input
-    (let ((polymer (coerce template 'list)))
-      (most-least-diff-after 10 polymer rules))))
+  (destructuring-bind (elem-counts pair-counts pair-expansions) input
+    (apply-pair-insertions 10
+                           (alexandria:copy-hash-table elem-counts)
+                           (alexandria:copy-hash-table pair-counts)
+                           (alexandria:copy-hash-table pair-expansions))))
+
+;; 40 steps of pair insertion
+(defmethod solve ((day (eql *day*)) (part (eql 2)) input)
+  (destructuring-bind (elem-counts pair-counts pair-expansions) input
+    (apply-pair-insertions 40
+                           (alexandria:copy-hash-table elem-counts)
+                           (alexandria:copy-hash-table pair-counts)
+                           (alexandria:copy-hash-table pair-expansions))))
 
 (let ((example (parse *day* "example")))
-  (assert (= (solve *day* 1 example) 1588)))
+  (assert (= (solve *day* 1 example) 1588))
+  (assert (= (solve *day* 2 example) 2188189693529)))
 
 (let ((input (parse *day* "input")))
   (when input
-    (format t "day~a-part1: ~a~%" *day* (solve *day* 1 input))))
+    (format t "day~a-part1: ~a~%" *day* (solve *day* 1 input))
+    (format t "day~a-part2: ~a~%" *day* (solve *day* 2 input))))
